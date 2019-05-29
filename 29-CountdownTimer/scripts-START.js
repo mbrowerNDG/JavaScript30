b0a9bf5e68fc49ef921b5849d83795eac87293cb
mbrowerNDG <mbrower@ndgcommunications.com>
2019-05-29 15:18:34 -0400

    last 30 by 30s

--- 29-CountdownTimer/scripts-START.js
+++ 29-CountdownTimer/scripts-START.js
@@ -1,12 +1,16 @@
  let countdown;
  const timerDisplay = document.querySelector('.display__time-left');
  const endTime = document.querySelector('.display__end-time')
+ const buttons = document.querySelectorAll('[data-time]');
+
 
  function timer(seconds) {
+     clearInterval(countdown);
      const now = Date.now();
      const then = now + seconds * 1000;
      displayTimeLeft(seconds);
-     constdown = setInterval(() => {
+     displayEndTime(then);
+     countdown = setInterval(() => {
          const secondsLeft = Math.round((then - Date.now()) / 1000);
          if (secondsLeft <= 0) {
              clearInterval(countdown);
@@ -28,5 +32,30 @@
  function displayEndTime(timestamp) {
      const end = new Date(timestamp);
      const hour = end.getHours();
-     const minutes = 'placehold'
+     const minutes = end.getMinutes();
+     endTime.textContent = `Be Back At ${hour > 12 ? hour - 12 : 12}:${minutes < 10 ? '0': ''}${minutes}`;
  }
+
+ function startTimer() {
+     const seconds = parseInt(this.dataset.time);
+
+     timer(seconds);
+
+ }
+
+
+ buttons.forEach(button => button.addEventListener('click', startTimer));
+ document.customForm.addEventListener('submit', function(e) {
+     e.preventDefault();
+     const mins = this.minutes.value;
+     console.log(mins);
+     if (mins > 0) {
+         timer(mins * 60);
+         this.reset();
+
+     } else {
+         console.log('pick a number fool');
+         endTime.textContent = 'Pick a number Fool'
+     }
+
+ });
